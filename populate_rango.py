2a56b4a2c4611ed962cd5a3de945a087bb91fbc8
“Davit <2840850M@student.gla.ac.uk>
2024-02-08 22:01:48 +0000

    Chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -1,9 +1,9 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-'tango_with_django_project.settings')
-
+                      'tango_with_django_project.settings')
 import django
 django.setup()
+
 from rango.models import Category, Page
 
 def populate():
@@ -35,41 +35,32 @@ def populate():
         {'title': 'Flask',
          'url': 'http://flask.pocoo.org'}]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views' : 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
- # If you want to add more categories or pages,
- # add them to the dictionaries above.
-
- # The code below goes through the cats dictionary, then adds each category,
- # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat,views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
- # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url = url
-    p.views = views
+    p.url=url
+    p.views=views
     p.save()
     return p
 
-
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     c.save()
     return c
 
 
- # Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
